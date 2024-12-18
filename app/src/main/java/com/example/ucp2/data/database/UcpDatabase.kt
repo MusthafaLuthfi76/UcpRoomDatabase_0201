efd2139b2c705ca55ec1836d02967dd8d977a0b0
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-18 10:25:02 +0700

    Menambahkan database UcpDatabase untuk menampung table dosen dan mahasiswa

---
+++ app/src/main/java/com/example/ucp2/data/database/UcpDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
+
+@Database(entities = [Dosen::class, MataKuliah::class], version = 1, exportSchema = false)
+abstract class UcpDatabase: RoomDatabase() {
+
+    //Mendefinisikan fungsi untuk mengakses data mahasiswa
+    abstract fun dosenDao(): DosenDao
+
+    companion object{
+        @Volatile //Memastikan bahwa nilai variabel instance selalu sama di setiap
+        private var Instance: UcpDatabase? = null
+
+        fun getDatabase(context: Context): UcpDatabase{
+            return (Instance?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    UcpDatabase::class.java, // Class Database
+                    "UcpDatabase" // Nama Database
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
